b6157341fffad910ef365a902b83fb63bc44e533
bmcho <bmcho91@naver.com>
2021-06-16 01:38:09 +0900

    64065

---
+++ Programers/64065.js
@@ -0,0 +1,38 @@
+//https://programmers.co.kr/learn/courses/30/lessons/64065
+
+const s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+
+function solution(s) {
+    var answer = [];
+    let arr = s.replace(/{{*|}}/g,'').split("},").map(v => v.split(','));
+    arr.sort((a,b) => a.length - b.length);
+
+    answer = arr.reduce((acc, cur) => {
+        let v = (cur.filter(v => !acc.includes(Number(v)))[0]);
+        acc.push(Number(v));
+        return acc;
+    }, []);
+
+    return answer;
+}
+
+solution(s);
+
+function solution1(s) {
+    const subsets = s.substring(1, s.length-2).split('},')
+    	.map(str=>str.substring(1).split(',')
+        .map(v=>Number(v)));
+ 
+    subsets.sort((a, b) => a.length - b.length);
+    console.log(subsets);
+ 
+    const ans = subsets.reduce((acc, subset) => {
+        const value = subset.filter(v => !acc.includes(v))[0];
+        acc.push(value);
+        return acc;
+    }, []);
+    
+    return ans;
+}
+
+solution1(s);
